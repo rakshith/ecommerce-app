b87ada2cbc5c6c7fb552f052b7c2776197731e2d
Rakshith Raj S <matrixrak@gmail.com>
2020-09-25 02:14:50 +0530

    header component added

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import "./header.styles.scss";
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" />
+    </div>
+  );
+};
+
+export default header.component;
