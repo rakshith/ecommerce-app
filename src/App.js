33c2558a0789406a6c5acddfefb592e2238a4d8d
Rakshith Raj S <matrixrak@gmail.com>
2020-09-25 02:07:46 +0530

    styling added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
 function App() {
   return (
     <div>
