4c8ea2f8968b536c68d62c268252ca5f02d4eba7
Rakshith Raj S <matrixrak@gmail.com>
2020-09-26 02:50:58 +0530

    moved our directory and shop dat into its own reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,7 +8,7 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((item, idx) => idx < 4) // display only 4 in a row
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
