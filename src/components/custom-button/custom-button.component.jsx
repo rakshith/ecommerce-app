1ad83e14ebbbe59b21386e2e78d5c45b519e6cc7
Rakshith Raj S <matrixrak@gmail.com>
2020-09-26 14:10:14 +0530

    styled components added

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,16 +1,9 @@
-import "./custom-button.styles.scss";
-
+// import "./custom-button.styles.scss";
+import { CustomButtonContainer } from "./custom-button.styles";
 import React from "react";
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
-  return (
-    <button
-      className={`${inverted ? 'inverted': ''}  ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+const CustomButton = ({ children, ...props }) => {
+  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
 };
 
 export default CustomButton;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,25 +1,13 @@
+import { HomePageContainer } from "./homepage.styles";
+
 import React from "react";
-import "./homepage.styles.scss";
+
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
-  <div className="homePage">
-    <div className="homepage">
-      {/* <div className="directory-menu">
-        <MenuItem title="HATS" subtitle="SHOP NOW" />
-
-        <MenuItem title="JACKETS" subtitle="SHOP NOW" />
-
-        <MenuItem title="SNEAKERS" subtitle="SHOP NOW" />
-
-        <MenuItem title="WOMENS" subtitle="SHOP NOW" />
-
-        <MenuItem title="MENS" subtitle="SHOP NOW" />
-      </div> */}
-
-      <Directory />
-    </div>
-  </div>
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
 );
 
 export default HomePage;
