0c06c0f7b95e972a9cf81e861cf4aab97223e906
Rakshith Raj S <matrixrak@gmail.com>
2020-09-28 14:34:19 +0530

    react lazy loading

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+
+import React from "react";
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
+
+export default Spinner;
--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,9 @@
-import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
-
 import React from "react";
 
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+import Spinner from "../spinner/spinner.component";
+
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
