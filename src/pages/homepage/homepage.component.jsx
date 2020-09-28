0839e34286a2a8c8fc3c0e4881b512123f761984
Rakshith Raj S <matrixrak@gmail.com>
2020-09-28 17:30:56 +0530

    performance optimized

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => {
-  throw Error;
-
-  // return (
-  //   <div className="homePage">
-  //     <div className="homepage">
-  //       <Directory />
-  //     </div>
-  //   </div>
-  // );
-};
+const HomePage = () => (
+  <div className="homePage">
+    <div className="homepage">
+      <Directory />
+    </div>
+  </div>
+);
 
 export default HomePage;
